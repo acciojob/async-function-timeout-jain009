861022afad78af89e978f07f9ce0ffe783d94bc4
Ankit jain <83234209+jain009@users.noreply.github.com>
2025-02-10 11:47:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+document.getElementById("btn").addEvevntListner("btn")(("click" => async function(){
+	const text = document.getELemenBytId("delay").value;
+	const delay = parseInt(document.getElementById("delay").value.,10);
+	const output = document.getELementById("output");
+
+	if(!text){
+		alert("please enter a text");
+		return;
+	}
+	if(!delay || delay < 0){
+		alert("please enter a valod delay in ms");
+	}
+	function delayMsg(ms){
+		new Promise{resolve => setTimeout(resolve, msg)};
+		await delayMsg(delay);
+	}
+		output.textContent = text;
+}))
